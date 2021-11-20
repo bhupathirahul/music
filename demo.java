5ec57abba4df3743bc25712a23cda3da18440b2d
bhupathirahul <80197116+bhupathirahul@users.noreply.github.com>
2021-11-20 21:46:35 +0530

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,11 @@
+public Class HelloWorld{
+
+     public static void main(String []args){
+         int x=3;
+         if(x%=0)
+        System.out.println("even number");
+        else
+        System.out.println("odd number");
+        
+     }
+}
